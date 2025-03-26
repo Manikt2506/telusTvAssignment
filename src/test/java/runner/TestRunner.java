8c06b83417667097aeaebf06e8f2335b63f03c6a
Manik Tandon <maniktandon@Maniks-MacBook-Air.local>
2025-03-26 16:52:46 -0400

    Updated TestRunner file

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,11 +1,17 @@
 package runner;
 
-
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+
 @RunWith(Cucumber.class)
+@CucumberOptions(strict = true, monochrome = true,
+        features = "src/test/resources/features/",
+        glue = {"stepdefinitions"},
+        plugin = {"pretty","junit:target/junitreport.xml","json:target/jsonreport.json","html:target/cucumber-reports"}
+
+)
 public class TestRunner {
 
     private TestRunner() {
